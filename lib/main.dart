fdb3bfbce39202828e64b66a06b1903a3ed46c13
RamzanCh056 <79650406+RamzanCh056@users.noreply.github.com>
2022-12-23 00:26:23 +0500

    user

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:locked_wallet/screens/adminAuth/login.dart';
 import 'package:locked_wallet/screens/landing.dart';
+import 'package:locked_wallet/screens/login_screen/login_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AdminLogin(),
+      home: LogInScreen(),
     ),
   );
 }
